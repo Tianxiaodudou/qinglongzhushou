636306943eb6634a9d6551ba1a85e35df206f940
Tianxiaodudou <96784933+Tianxiaodudou@users.noreply.github.com>
2026-05-24 05:04:30 +0800

    fix: Hilt 2.48.1 兼容 Gradle 8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,9 +89,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.4")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.52")
-    kapt("com.google.dagger:hilt-android-compiler:2.52")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Network
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
